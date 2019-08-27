426853c4c5254a9198bed4846a83c40452ab9cc0
Rishi Kaul <rishikaul@icloud.com>
2019-08-27 22:59:17 +0530

    Initial Commit

---
+++ TechRace 2K19/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TechRace 2K19
+//
+//  Created by Rishi Kaul on 27/08/19.
+//  Copyright © 2019 Rishi Kaul. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TechRace 2K19/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TechRace 2K19
+//
+//  Created by Rishi Kaul on 27/08/19.
+//  Copyright © 2019 Rishi Kaul. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
